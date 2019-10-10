6df48c33e71c4aba39babffca21b9716c9ba534a
Danil Romero <daniel.romero.barria@gmail.com>
2019-10-10 11:58:12 -0300

    fix ios 10, activityIndicator large for gray

--- CountThings/CountThings/Extensions/Extensions.swift
+++ CountThings/CountThings/Extensions/Extensions.swift
@@ -58,8 +58,13 @@ public extension ActivityIndicatorPresenter where Self: UIViewController
     
     DispatchQueue.main.async
       {
-      
-        self.activityIndicator.style = .large
+        
+        if #available(iOS 13.0, *) {
+              self.activityIndicator.style = .large
+        } else {
+              self.activityIndicator.style = .gray
+        }
+        
         self.activityIndicator.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
         self.activityIndicator.backgroundColor = UIColor(red:0.16, green:0.17, blue:0.21, alpha:1)
         self.activityIndicator.layer.cornerRadius = 6
